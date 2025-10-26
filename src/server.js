2f7fa42218abd7d7740b6b4889de39503b7f2444
jessmaeeGit <jessamaecutillasbigcas@gmail.com>
2025-10-27 01:53:37 +0800

    modified: src/db.js modified: package.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from './src/routes/users.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
